6b24d9768cf47b6f211a1a6a84977fdb2c5fbb75
Léontine Gioia Tschannen <117348900+tschannenl-bzz@users.noreply.github.com>
2024-11-27 15:56:14 +0100

    git not working

---
+++ trafficlight.test.js
@@ -0,0 +1,20 @@
+const app = require("./trafficlight");
+
+describe('test suite traffic light', () => {
+    it('case red light', () => {
+        expect(app.trafficLight("red")).toBe("DON'T WALK!");
+    });
+
+    it('case orange light', () => {
+        expect(app.trafficLight("orange")).toBe("ATTENTION!");
+    });
+
+    it('case green light', () => {
+        expect(app.trafficLight("green")).toBe("WALK!");
+    });
+
+    it('case undefined light', () => {
+        expect(app.trafficLight()).toBe("OUT OF ORDER");
+    });
+});
+
